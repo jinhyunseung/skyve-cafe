af3773dd2aab5c7f81558b77fe2649237a640ae8
jinhyunseung <www4029@gmail.com>
2022-12-04 14:29:35 +0900

    db

--- src/main/resources/db/h2/data.sql
+++ src/main/resources/db/h2/data.sql
@@ -1,13 +1,13 @@
-INSERT INTO vets VALUES (default, 'James', 'Carter');
-INSERT INTO vets VALUES (default, 'Helen', 'Leary');
-INSERT INTO vets VALUES (default, 'Linda', 'Douglas');
-INSERT INTO vets VALUES (default, 'Rafael', 'Ortega');
-INSERT INTO vets VALUES (default, 'Henry', 'Stevens');
-INSERT INTO vets VALUES (default, 'Sharon', 'Jenkins');
+INSERT INTO vets VALUES (default, 'BETTERMONDAY', 'CentumCity');
+INSERT INTO vets VALUES (default, 'ADEN', 'CentumCity');
+INSERT INTO vets VALUES (default, 'SmallGood', 'CentumCity');
+INSERT INTO vets VALUES (default, 'ALOT', 'Millak');
+INSERT INTO vets VALUES (default, 'HIO', 'Suyeong');
+INSERT INTO vets VALUES (default, 'EDIYA', 'Suyeong');
 
-INSERT INTO specialties VALUES (default, 'radiology');
-INSERT INTO specialties VALUES (default, 'surgery');
-INSERT INTO specialties VALUES (default, 'dentistry');
+INSERT INTO specialties VALUES (default, '~5min');
+INSERT INTO specialties VALUES (default, '~10min');
+INSERT INTO specialties VALUES (default, '~15min');
 
 INSERT INTO vet_specialties VALUES (2, 1);
 INSERT INTO vet_specialties VALUES (3, 2);
@@ -22,23 +22,23 @@ INSERT INTO types VALUES (default, 'snake');
 INSERT INTO types VALUES (default, 'bird');
 INSERT INTO types VALUES (default, 'hamster');
 
-INSERT INTO owners VALUES (default, 'George', 'Franklin', '110 W. Liberty St.', 'Madison', '6085551023');
-INSERT INTO owners VALUES (default, 'Betty', 'Davis', '638 Cardinal Ave.', 'Sun Prairie', '6085551749');
-INSERT INTO owners VALUES (default, 'Eduardo', 'Rodriquez', '2693 Commerce St.', 'McFarland', '6085558763');
-INSERT INTO owners VALUES (default, 'Harold', 'Davis', '563 Friendly St.', 'Windsor', '6085553198');
-INSERT INTO owners VALUES (default, 'Peter', 'McTavish', '2387 S. Fair Way', 'Madison', '6085552765');
-INSERT INTO owners VALUES (default, 'Jean', 'Coleman', '105 N. Lake St.', 'Monona', '6085552654');
+INSERT INTO owners VALUES (default, 'Jin', 'Hyunseung', 'Americano', 'No Plastic', '24984025');
+INSERT INTO owners VALUES (default, 'Choi', 'Juran', 'Cafe latte', 'Ice', '94682188');
+INSERT INTO owners VALUES (default, 'Kim', 'Yuchang', 'Americano', 'Ice', '95217416');
+INSERT INTO owners VALUES (default, 'Choi', 'Yijeong', 'Cafe Mocha', 'Hot', '60855319');
+INSERT INTO owners VALUES (default, 'Park', 'Hyogwan', 'orange juice', 'No Ice', '94133645');
+INSERT INTO owners VALUES (default, 'Jeon', 'dongjin', 'Cafe Mocha', 'Hot', '68552654');
 INSERT INTO owners VALUES (default, 'Jeff', 'Black', '1450 Oak Blvd.', 'Monona', '6085555387');
 INSERT INTO owners VALUES (default, 'Maria', 'Escobito', '345 Maple St.', 'Madison', '6085557683');
 INSERT INTO owners VALUES (default, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Madison', '6085559435');
 INSERT INTO owners VALUES (default, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', '6085555487');
 
-INSERT INTO pets VALUES (default, 'Leo', '2010-09-07', 1, 1);
-INSERT INTO pets VALUES (default, 'Basil', '2012-08-06', 6, 2);
-INSERT INTO pets VALUES (default, 'Rosy', '2011-04-17', 2, 3);
-INSERT INTO pets VALUES (default, 'Jewel', '2010-03-07', 2, 3);
-INSERT INTO pets VALUES (default, 'Iggy', '2010-11-30', 3, 4);
-INSERT INTO pets VALUES (default, 'George', '2010-01-20', 4, 5);
+INSERT INTO pets VALUES (default, '10', '2010-09-07', 1, 1);
+INSERT INTO pets VALUES (default, '6', '2012-08-06', 6, 2);
+INSERT INTO pets VALUES (default, '5', '2011-04-17', 2, 3);
+INSERT INTO pets VALUES (default, '15', '2010-03-07', 2, 3);
+INSERT INTO pets VALUES (default, '9', '2010-11-30', 3, 4);
+INSERT INTO pets VALUES (default, '3', '2010-01-20', 4, 5);
 INSERT INTO pets VALUES (default, 'Samantha', '2012-09-04', 1, 6);
 INSERT INTO pets VALUES (default, 'Max', '2012-09-04', 1, 6);
 INSERT INTO pets VALUES (default, 'Lucky', '2011-08-06', 5, 7);
